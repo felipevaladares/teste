b70ac3e1c792c9f507eaacbd82fef28c6809b50f
Felipe Valadares <felipe.valadares@psafe.com>
2019-09-13 12:12:45 -0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ui/login/LoginActivity.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication.ui.login
+
+import android.app.Activity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+
+import com.example.myapplication.R
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var loginViewModel: LoginViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
